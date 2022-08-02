83eb904e6550f2d7a541d21ba6186ac71a2769b8
huthaifa <huthaifazmi@gmail.com>
2022-08-02 17:36:15 +0300

    added fb login plugin

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
+import {renderFbLoginPlugin} from "../plugins/fbLogin";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Component {...pageProps} />
+    {renderFbLoginPlugin()}
+  </>
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
+import Script from 'next/script'
 export default function Home() {
   return (
-    <h1>
+    <>
+
+      <h1>
       Test
-    </h1>)
+    </h1>
+      <a herf="https://fb-page-pr.herokuapp.com/flogin">Login with Facebook</a>
+    </>)
 }
