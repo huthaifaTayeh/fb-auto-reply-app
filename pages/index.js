1ca91040a1b3a95554656f13e9ef5f9125a7bb21
huthaifa <huthaifazmi@gmail.com>
2022-08-03 13:25:11 +0300

    added fb login plugin

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,34 @@ export default function Home() {
           console.log(JSON.stringify(response));         
           setPages(response);
         });
+        (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+      function statusChangeCallback(response) {
+                console.log('statusChangeCallback');
+                console.log(response);
+                // The response object is returned with a status field that lets the
+                // app know the current login status of the person.
+                // Full docs on the response object can be found in the documentation
+                // for FB.getLoginStatus().
+                if (response.status === 'connected') {
+                    // Logged into your app and Facebook.
+                    console.log('Welcome!  Fetching your information.... ');
+                    FB.api('/me', function (response) {
+                        console.log('Successful login for: ' + response.name);
+                        document.getElementById('status').innerHTML =
+                          'Thanks for logging in, ' + response.name + '!';
+                    });
+                } else {
+                    // The person is not logged into your app or we are unable to tell.
+                    document.getElementById('status').innerHTML = 'Please log ' +
+                      'into this app.';
+                }
+            }
 `}
       </Script>
       <div className="fb-login-button" data-width="200" data-size="large" data-button-type="login_with"
