c4b0770e39b7a65e746904f25e9a78fa41f102b7
huthaifa <huthaifazmi@gmail.com>
2022-08-04 11:35:15 +0300

    Minor changes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-import Script from 'next/script';
-import {useState, useEffect} from "react";
+import {useEffect, useState} from "react";
 
 export default function Home() {
   const [isConnected, setIsConnected] = useState(false)
@@ -7,21 +6,26 @@ export default function Home() {
 
   useEffect(() => {
 
-    if(typeof window !== "undefined"){
-      if(typeof localStorage.getItem("hasBeenFetched") !== 'undefined'){
+    if (typeof window !== "undefined") {
+      if (typeof localStorage.getItem("hasBeenFetched") !== 'undefined') {
         location.reload();
       }
       const accessToken = localStorage.getItem('AccessToken')
       const facebookPage = localStorage.getItem('Pages')
       const userID = localStorage.getItem('UserID')
       console.dir({accessToken, facebookPage, userID})
+      if (typeof facebookPage !== 'undefined') {
+        setPages(prevState => prevState.concat(facebookPage));
+        console.log("Pages are:\n");
+        console.log(JSON.stringify(facebookPage));
+      }
     }
   }, [])
 
   return (
     <>
       {!isConnected && <div className="fb-login-button" data-width="200" data-size="large" data-button-type="login_with"
-            data-layout="rounded" data-auto-logout-link="false" data-use-continue-as="true"></div>}
+                            data-layout="rounded" data-auto-logout-link="false" data-use-continue-as="true"></div>}
 
       {/*{pages.length > 0 && pages.map(page => <h2 key={page.id}>{page.name}</h2>)}*/}
     </>)
