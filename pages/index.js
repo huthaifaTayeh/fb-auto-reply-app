545650677d9d6e52401b7d1745212cc3aa53bec4
huthaifa <huthaifazmi@gmail.com>
2022-08-04 15:15:45 +0300

    Minor changes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-import {useEffect, useState} from "react";
+import {Fragment, useEffect, useState} from "react";
 import {getFbPages} from "../utils/APIs";
+import Script from "next/script";
 
 export default function Home() {
   const [isConnected, setIsConnected] = useState(false)
@@ -30,10 +31,35 @@ export default function Home() {
   }, [])
 
   return (
-    <>
-      {!isConnected && <div className="fb-login-button" data-width="200" data-size="large" data-button-type="login_with"
-                            data-layout="rounded" data-auto-logout-link="false" data-use-continue-as="true"></div>}
-
+    <Fragment>
+      {/*{!isConnected && <div className="fb-login-button" data-width="200" data-size="large" data-button-type="login_with"*/}
+      {/*                      data-layout="rounded" data-auto-logout-link="false" data-use-continue-as="true"></div>}*/}
+      <Script>
+        {`var finished_rendering = function() {
+          console.log("finished rendering plugins");
+          var spinner = document.getElementById("spinner");
+          spinner.removeAttribute("style");
+          spinner.removeChild(spinner.childNodes[0]);
+        }
+          FB.Event.subscribe('xfbml.render', finished_rendering);
+        `}      </Script>
+      <div id="spinner"
+           style="
+        background: #4267b2;
+        border-radius: 5px;
+        color: white;
+        height: 40px;
+        text-align: center;
+        width: 250px;">
+        Loading
+        <div
+          className="fb-login-button"
+          data-max-rows="1"
+          data-size="large"
+          data-button-type="continue_with"
+          data-use-continue-as="true"
+        ></div>
+      </div>
       {pages.length > 0 && pages.map(page => <h2 key={page.id}>{page.name}</h2>)}
-    </>)
+    </Fragment>)
 }
