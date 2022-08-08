88e2efeb5f6668d58f5c6c15101af3261679351b
huthaifa <huthaifazmi@gmail.com>
2022-08-08 12:49:33 +0300

    parsing object from server data

--- pages/confirmPage.js
+++ pages/confirmPage.js
@@ -2,7 +2,7 @@ import StyleClasses from "../styles/Home.module.css";
 import { useRouter } from 'next/router'
 const FinalPage = () => {
   const router = useRouter()
-  const routerData = router.query.data
+  const routerData = router.query.data.toString()
   const page = JSON.parse(routerData);
   console.log(page)
   const subscribePageToApp = () => {
